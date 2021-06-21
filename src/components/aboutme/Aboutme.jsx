60b296bdd7b900c68cd860a81cbfbb2e400fcd40
cherish <cherishpatel2001@gmail.com>
2021-06-21 13:19:51 +0530

    about me section complete

--- src/components/aboutme/Aboutme.jsx
+++ src/components/aboutme/Aboutme.jsx
@@ -1,10 +1,23 @@
 import React from "react"
 import './Aboutme.css'
+import avtar from './avtar.png'
 
 const Aboutme = () => {
     return (
-        <div className='aboutme' id='aboutme'>
-            
+        <div className='aboutme ' id='aboutme'>
+            <div className="container aboutmewrapper">
+                <div className="avtar">
+                    <img src={avtar} alt="avtar" />
+                </div>
+                <div className="details">
+                    <p className="text1">About me</p>
+                    <p className="text2">
+                        Belongs form BTech.IT background. <br></br>
+                        A dedicated and passionate web devloper and python devloper. <br/>
+                        Currently working alone but looking for people who can colobrate. <br/>  
+                        Love to code, Love to colobrate.                  </p>
+                </div>
+            </div>
         </div>
     )
 }
